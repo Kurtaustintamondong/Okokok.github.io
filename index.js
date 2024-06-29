78a3e0e00defbe3dde67fd1d819fcba93f1bd2ac
Kurtaustintamondong <renielcastro00@gmail.com>
2024-06-29 19:06:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,6 @@ var product2 = document.getElementById("product2");
 var qty2 = document.getElementById("qty2");
 var price2 = document.getElementById("price2");
 
-var product3 = document.getElementById("product3");
-var qty3 = document.getElementById("qty3");
-var price3 = document.getElementById("price3");
-
-var product4 = document.getElementById("product4");
-var qty4 = document.getElementById("qty4");
-var price4 = document.getElementById("price4");
-
-var product5 = document.getElementById("product5");
-var qty5 = document.getElementById("qty5");
-var price5 = document.getElementById("price5");
-
 var carts = document.getElementById("carts");
 var totalInput = document.getElementById("total");
 var cashInput = document.getElementById("cash");
@@ -35,30 +23,12 @@ function addOrder() {
         var order2 = qty2.value.toString() + " pcs x " + product2.textContent + " - Php " + (parseFloat(qty2.value) * parseFloat(price2.textContent)).toFixed(2) + "\n";
         carts.textContent += order;
     }
-
-    if (parseFloat(qty3.value) > 0) {
-        var order2 = qty3.value.toString() + " pcs x " + product3.textContent + " - Php " + (parseFloat(qty3.value) * parseFloat(price3.textContent)).toFixed(2) + "\n";
-        carts.textContent += order;
-    }
-
-    if (parseFloat(qty4.value) > 0) {
-        var order2 = qty4.value.toString() + " pcs x " + product4.textContent + " - Php " + (parseFloat(qty4.value) * parseFloat(price4.textContent)).toFixed(2) + "\n";
-        carts.textContent += order;
-    }
-
-    if (parseFloat(qty5.value) > 0) {
-        var order2 = qty5.value.toString() + " pcs x " + product5.textContent + " - Php " + (parseFloat(qty5.value) * parseFloat(price5.textContent)).toFixed(2) + "\n";
-        carts.textContent += order;
-    }
     updateTotal(); // Update total after adding orders
 }
 function updateTotal() {
     var total = 0;
     total += parseFloat(qty1.value) * parseFloat(price1.textContent);
     total += parseFloat(qty2.value) * parseFloat(price2.textContent);
-    total += parseFloat(qty3.value) * parseFloat(price3.textContent);
-    total += parseFloat(qty4.value) * parseFloat(price4.textContent);
-    total += parseFloat(qty5.value) * parseFloat(price5.textContent);
     totalInput.value = total.toFixed(2); // Update total input field
     calculateChange(); // Calculate change after updating total
 }
@@ -75,7 +45,4 @@ function calculateChange() {
 }
 qty1.addEventListener("keyup", addOrder);
 qty2.addEventListener("keyup", addOrder);
-qty3.addEventListener("keyup", addOrder);
-qty4.addEventListener("keyup", addOrder);
-qty5.addEventListener("keyup", addOrder);
 cashInput.addEventListener("input", calculateChange);
